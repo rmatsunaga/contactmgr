2617e55246367945f7cd6834056c102fc7abda24
Rey Matsunaga <matsunagar97@gmail.com>
2019-04-26 14:45:44 -0700

    tried  initializingsession

--- src/main/java/com/reymatsunaga/contactmgr/Application.java
+++ src/main/java/com/reymatsunaga/contactmgr/Application.java
@@ -2,6 +2,7 @@ package com.reymatsunaga.contactmgr;
 
 import com.reymatsunaga.contactmgr.model.Contact;
 import com.reymatsunaga.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -9,7 +10,7 @@ import org.hibernate.service.ServiceRegistry;
 
 public class Application {
     // Hold a reusable reference to a session factory
-    //private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
     public static SessionFactory buildSessionFactory() {
         // Create a standard service registry object
@@ -21,6 +22,15 @@ public class Application {
                 .withEmail("reymats@gmail.com")
                 .withPhone(2068054212L)
                 .build();
-        System.out.println(contact);
+        // Open a session
+        Session session = sessionFactory.openSession();
+        // open a transaction
+        session.beginTransaction();
+        // use session to save contact
+        session.save(contact);
+        // commit the transaction
+        session.getTransaction().commit();
+        // close the session
+        session.close();
     }
 }
